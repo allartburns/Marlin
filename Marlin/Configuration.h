95f71edfb930217156d5c45ffd962546015c1ca0
J. Eric Townsend (jet) <jet@flatline.net>
2020-06-07 17:08:10 -0400

    fix defining PREHEAT_2 values a second time

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1500,10 +1500,10 @@
 #define PREHEAT_2_TEMP_BED    110
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "EDGE"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    100
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_3_LABEL       "EDGE"
+#define PREHEAT_3_TEMP_HOTEND 240
+#define PREHEAT_3_TEMP_BED    100
+#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
 
 /**
  * Nozzle Park
