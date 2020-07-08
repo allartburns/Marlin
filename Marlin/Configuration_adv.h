b40a346d08f34b284152f6b859359b822faa1b1c
J. Eric Townsend (jet) <jet@flatline.net>
2020-07-07 21:49:46 -0400

    first set of changes for E3D BigBox Hybrid.  Builds and launches, still nees a lot of testing.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -449,8 +449,8 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN -1
-#define E1_AUTO_FAN_PIN -1
+#define E0_AUTO_FAN_PIN FAN1_PIN
+#define E1_AUTO_FAN_PIN FAN1_PIN
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
 #define E4_AUTO_FAN_PIN -1
@@ -459,7 +459,9 @@
 #define E7_AUTO_FAN_PIN -1
 #define CHAMBER_AUTO_FAN_PIN -1
 
-#define EXTRUDER_AUTO_FAN_TEMPERATURE 50
+// I turn off the extruders fans at 35 to make tool changes quicker,
+// my office hits 30C in the summer or I'd go lower.
+#define EXTRUDER_AUTO_FAN_TEMPERATURE 35
 #define EXTRUDER_AUTO_FAN_SPEED 255   // 255 == full speed
 #define CHAMBER_AUTO_FAN_TEMPERATURE 30
 #define CHAMBER_AUTO_FAN_SPEED 255
